89d565d81f3d6d34353c134ab63c888f17ca424b
GusGo <gus.gonzalez.15@gmail.com>
2019-12-16 19:50:37 -0500

    fixed validation of user input. working on loop that creates the random password

--- javascript.js
+++ javascript.js
@@ -14,7 +14,7 @@ function userInput() {
     var userSpclChars = confirm("Do you want your password to contain special characters?");
     var userNums = confirm("Do you want your password to contain numbers?");
     var userLower = confirm("Do you want your password to contain lowercase letters?");
-    var userUpper = confirm("Do you want your password to contain special characters?");
+    var userUpper = confirm("Do you want your password to contain uppercase letters?");
 
     // Store the user inputted length and character types in array
     var userChoices = [userLength, userSpclChars, userNums, userLower, userUpper];
@@ -31,7 +31,8 @@ function validateUserChoices(inputArray) {
 
     // Validates that input is between 8 and 128 characters
     var lengthNum = parseInt(inputArray[0]);
-    if (lengthNum === NaN) {
+    console.log(lengthNum);
+    if (Number.isNaN(lengthNum)) {
         return false;
     } else if ((lengthNum > 128) || (lengthNum < 8)) {
         return false;
@@ -44,6 +45,9 @@ function validateUserChoices(inputArray) {
     if (!atLeastOne) {
         return null;
     }
+    else {
+        return true;
+    }
 }
 
 // Creating function for error alerts to minimize clutter in generate password function
@@ -54,23 +58,95 @@ function errorNeedAtLeastOne() {
     alert("You must select at least one character type! Please press generate password button to try again.")
 }
 
+// Summation function found via google fu. Sums items in an array
+function arrSum(arr) {
+    return arr.reduce(function(a,b) {
+      return a + b;
+    }, 0);
+  }
+
+// Tests for my summation function
+// var array = [true, false, true, false];
+// var array2 = [5, 6, 7];
+// var array = [-1, 6, true];
+// console.log(arrSum(array));
+// console.log(arrSum(array2));
+
+// Generates random password using user input array of criteria
+function generate(userInputArray) {
+
+    // Grabs length and turns into a number
+    var length = parseInt(userInputArray[0]);
+
+    // Saving the user character type choices
+    var charTypes = [userInputArray[1], userInputArray[2], userInputArray[3], userInputArray[4]];
+
+    var options = [specialChars, numbers, lowercase, uppercase];
+    var choices = [];
+
+    // Goes thru ands adds user choices to an array
+    for (var i = 0; i < 4; i++) {
+        if (charTypes[i]) {
+            choices.push((i+1));
+            console.log(choices);
+        }
+    }
+
+    // Loop to create password
+    var password = "";
+    for (var x = 0; x < length; x++) {
+        console.log(choices.length);
+        var randomCharacterType = Math.floor(Math.random() * (choices.length - 1));
+        console.log(randomCharacterType);
+    }
+    
+
+
+
+
+
+
+}
+
+
+// 26 special characters as string
+var specialChars = "!#$%&()*+,-./:;<=>?@^_{|}~" ;
+// 10 numbers as a string
+var numbers = "0123456789"
+// 26 lowercase letters as string
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+// 26 uppercase letters as string
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
 // Create password generator function that is called on button click
 function generatePassword() {
 
     // Prompting user and storing user input 
     var userInputArray = userInput();
 
+    console.log(userInputArray);
     // Validating user inputs. 
-    isValid = validateUserChoices(userInputArray);
+    var isValid = validateUserChoices(userInputArray);
 
-    // If isvalid is false, then the length is invalid. throw error
+    console.log(isValid);
+
+    // If isvalid is false, then the length is invalid. alert error message
     if (isValid === null) {
         return errorNeedAtLeastOne();
     }
-    // If isvalid is null, then at least one user type was not selected. throw error
+    // If isvalid is null, then at least one user type was not selected. alert error
     else if (!isValid) {
         return errorNeedLength();
     }
+
+    // Now that I know I have a valid length and at least one special character, I can call my generate function, which takes in the input array and returns the password 
+
+    password = generate(userInputArray);
+
+
+
+
+
 
 
 
